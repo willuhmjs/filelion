fb9c0793796bc36ef901a906cdf3f2ba990707e3
Will Faircloth <willfaircloth@icloud.com>
2021-01-09 14:38:51 -0500

    Create GET /signin

--- project/routes/main.js
+++ project/routes/main.js
@@ -5,3 +5,9 @@ module.exports = app;
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+app.get("/signin", (req, res) => {
+  let {auth} = req.cookies;
+  if (auth) return res.redirect("/files");
+  res.render("signin");
+})

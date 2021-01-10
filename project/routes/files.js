0e6a7af567b2c52782b8c9e1da4e0cbdf452de90
Will Faircloth <willfaircloth@icloud.com>
2021-01-10 17:09:24 -0500

    Create checkIfAuthorized function, use in /files/

--- project/routes/files.js
+++ project/routes/files.js
@@ -2,7 +2,18 @@ const { Router } = require("express");
 const app = Router();
 module.exports = app;
 
+const {pin} = require("../config.js");
+const md5 = require("md5");
+
+function checkIfAuthorized(req, res) {
+  let {auth} = req.cookies;
+  if (!auth) return res.redirect("/signin");
+  let pinHashed = md5(pin);
+  if (pinHashed !== auth) return res.redirect("/signout");
+}
 
 app.get("/", (req, res) => { // /files/
   // todo do stuff
+  checkIfAuthorized(req, res);
+  res.send("You are authorized");
 })
